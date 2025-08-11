3212b34299445cc3cb1508c386940876bf599905
Raya Petkova <rayapetkova2007@abv.bg>
2025-08-11 10:50:43 +0300

    Add examples file

---
+++ lesson_2/examples.py
@@ -0,0 +1,12 @@
+name = "Рая"
+age = 18
+
+print(name)  # ще покаже Рая
+print(age)  # ще покаже 18
+
+name = "Алекс"
+
+print(name)  # ще покаже Алекс
+
+
+favourite_animal = "dog"  # Snake case --> използваме долни черти вместо интервали
