0923e907a09230e0a4c90d35c46fffe762609394
Raya Petkova <rayapetkova2007@abv.bg>
2025-08-11 10:50:53 +0300

    Add task 1 - lesson2

---
+++ lesson_2/task_1.py
@@ -0,0 +1,9 @@
+hero_name = "Батман"  # string --> текст
+age = 20  # integer --> цяло число
+has_cape = True  # boolean --> булева стойност --> True / False (да / не)
+power = "летене"  # string --> текст
+power_level = 7  # integer --> цяло число
+
+print(f"Моят супергерой се казва {hero_name}. Той е на {age} години.")
+print(f"Той има ли наметало?: {has_cape}")
+print(f"Неговата суперсила е {power}, а нивото на суперсилата му е {power_level}.")
