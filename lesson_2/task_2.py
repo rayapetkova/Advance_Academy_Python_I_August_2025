8494c235a803e684b9b33c77061acc64013ad577
Raya Petkova <rayapetkova2007@abv.bg>
2025-08-11 11:15:23 +0300

    Add task 2

---
+++ lesson_2/task_2.py
@@ -0,0 +1,6 @@
+school_name = "ЕГ 'Проф. д-р Асен Златаров'"
+students_count = 870
+is_fun = True
+
+print(f"Моето училище е {school_name}. В него има {students_count} ученици.")
+print(f"Това училище забавно ли е?: {is_fun}")
