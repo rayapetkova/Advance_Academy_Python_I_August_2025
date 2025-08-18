e5307ba21475f4b61d5a009cbd3729e521a0334b
Raya Petkova <rayapetkova2007@abv.bg>
2025-08-18 11:09:40 +0300

    Update examples file

--- lesson_3/examples.py
+++ lesson_3/examples.py
@@ -1,8 +1,26 @@
-name = input("Как се казваш?: ")
+# name = input("Как се казваш?: ")
+#
+# print(f"Отговорът на въпроса е: {name}")
+#
+#
+# age = int(input("На колко си години?: "))
+# age_after_2_years = age + 2
+# print(f"След 2 години ти ще бъдеш на {age_after_2_years} години.")
 
-print(f"Отговорът на въпроса е: {name}")
+
+# Условни конструкции
+# is_raining = True
+#
+# if is_raining:  # ако вали
+#     print("Навън вали.")
+# else:  # иначе
+#     print("Навън НЕ вали.")
 
 
-age = int(input("На колко си години?: "))
-age_after_2_years = age + 2
-print(f"След 2 години ти ще бъдеш на {age_after_2_years} години.")
+number = 1
+if number > 2:
+    print("Числото е по-голямо от 2.")
+elif number == 2:
+    print("Числото е равно на 2.")
+else:
+    print("Числото е по-малко от 2.")
