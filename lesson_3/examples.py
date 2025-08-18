c8994a909d5b535b6bb375b4680659e341b4ec9b
Raya Petkova <rayapetkova2007@abv.bg>
2025-08-18 10:27:57 +0300

    Add examples file - lesson 3

---
+++ lesson_3/examples.py
@@ -0,0 +1,8 @@
+name = input("Как се казваш?: ")
+
+print(f"Отговорът на въпроса е: {name}")
+
+
+age = int(input("На колко си години?: "))
+age_after_2_years = age + 2
+print(f"След 2 години ти ще бъдеш на {age_after_2_years} години.")
